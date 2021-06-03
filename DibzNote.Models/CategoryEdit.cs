2248670a19a44f7693c1c6f2145411192097becb
Ryan <Dibzthedibz@gmail.com>
2021-06-02 18:27:26 -0700

    Category fully added and functional.

--- DibzNote.Models/CategoryEdit.cs
+++ DibzNote.Models/CategoryEdit.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DibzNote.Models
 {
-    public class CategoryDetail
+    public class CategoryEdit
     {
         public int CategoryId { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Title must be more than 2 chars.")]
+        [MaxLength(16, ErrorMessage = "Title must be less than 16 chars.")]
         public string Title { get; set; }
-        public List<NoteListItem> Notes { get; set; }
     }
 }
--- DibzNote.Services/CategoryService.cs
+++ DibzNote.Services/CategoryService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DibzNote.Data;
+using DibzNote.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,92 @@ using System.Threading.Tasks;
 
 namespace DibzNote.Services
 {
-    class CategoryService
+    public class CategoryService
     {
+        public bool CreateCategory(CategoryCreate model)
+        {
+            var entity =
+                new Category()
+                {
+                    Title = model.Title
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Categories.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public IEnumerable<CategoryListItem> GetCategories()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Categories
+                        .Select(
+                            e =>
+                            new CategoryListItem
+                            {
+                                CategoryId = e.CategoryId,
+                                Title = e.Title
+                            }
+                            );
+                return query.ToArray();
+            }
+        }
+        public CategoryDetail GetCategoryById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Categories
+                        .Single(e => e.CategoryId == id);
+
+                return new CategoryDetail
+                {
+                    CategoryId = entity.CategoryId,
+                    Title = entity.Title,
+                    Notes = entity.Notes
+                    .Select(e => new NoteListItem()
+                    {
+                        NoteId = e.NoteId,
+                        Title = e.Title,
+                        CreatedUtc = e.CreatedUtc,
+                        CategoryId = e.CategoryId
+                    }).ToList()
+                };
+            }
+        }
+
+        public bool UpdateCategory(CategoryEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Categories
+                    .Single(e => e.CategoryId == model.CategoryId);
+
+                entity.Title = model.Title;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteCategory(int categoryId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Categories
+                    .Single(e => e.CategoryId == categoryId);
+
+                ctx.Categories.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
